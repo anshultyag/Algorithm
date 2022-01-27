c7e02bdd2918dafb2abb0fcf9a7b0861e2458bd1
anshultyag <you@example.com>
2022-01-27 20:41:41 +0530

    Added:two string are equal

--- src/com/company/TwoString.java
+++ src/com/company/TwoString.java
@@ -2,45 +2,25 @@ package com.company;
 import java.util.Arrays;
 public class TwoString {
 
-    static void printPermutation(String str, String ans)
-    {
-             // If string is empty
-        if (str.length() == 0) {
+    static void printPermutation(String str, String ans){
+        // If string is empty
+        if (str.length() == 0){
             System.out.print(ans + " ");
             return;
         }
-
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);                     // ith character of str
             String leftSide = str.substring(0, i);
-            String rightSide= str.substring(i + 1);
+            String rightSide = str.substring(i + 1);
             String ros = leftSide + rightSide;
-            printPermutation(ros, ans + ch );        // Recurvise call
+            printPermutation(ros, ans + ch);        // Recurvise call
         }
     }
 
-
-
-    public static boolean equal(char[] str1, char[] str2) {
-        int n1 = str1.length;
-        int n2 = str2.length;
-        if (n1 != n2) {
-            return false;
-        }
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        for (int i = 0; i < n1; i++)
-            if (str1[i] != str2[i])
-                return false;
-        return true;
-    }
     public static void main(String args[]) {
-            String s1 = "abcd";
-            String s2 = "dcba";
-            printPermutation(s2, " ");
-            if (equal(s1.toCharArray(), s2.toCharArray()))
-                System.out.println("\n"+"The two strings are equal of each other");
-            else
-                System.out.println("\n" +"The two strings are not equal of each other");
-            }
+        String s1 = "abc";
+        String s2 = "dcba";
+        printPermutation(s1, " ");
+        printPermutation(s2, " ");
     }
+}
